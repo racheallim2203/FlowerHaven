3a05fcdb9611ba9d3db876db189a847f818b180f
Mark Sibulo <msib0008@student.monash.edu>
2024-04-30 22:14:33 +1000

    added templates for missing controller and missing template

---
+++ templates/Error/missing_controller.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+<header class="site-header section-padding-img site-header-image front-product">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-12 header-info">
+                <h1>
+                    <span class="d-block text-dark" >Page Not Found</span>
+                    <p>The page you were looking for was not found</p>
+                    <a href="<?= $this->Url->build(['controller' => 'Pages', 'action' => 'display', 'home']); ?>" class="btn custom-btn">Return to Home</a>
+                </h1>
+            </div>
+        </div>
+    </div>
+</header>
