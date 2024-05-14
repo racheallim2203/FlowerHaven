a6781d41891245644f79d68d4f9ab8832f16eec8
ache0100 <ache0100@student.monash.edu>
2024-05-14 12:47:49 +1000

    added navigation to the customer view page in the navbar of admin dashboard and added comments in default file

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -19,9 +19,11 @@ $this->Flash->render();
 </head>
 
 <body id="reportsPage">
+<!-- Navbar specifications for admin dashboard -->
 <div class="container-fluid">
     <div class="w-auto px-2">
         <div class="navbar navbar-expand-xl navbar-light bg-light">
+            <!-- Content Block for logo's image -->
             <a class="navbar-brand" href="<?= $this->Url->build('/') ?>">
                 <?= $this->ContentBlock->image('Logo', ['style' => 'height: 60px', 'url'=> ['controller' => 'Pages', 'action' => 'display']]); ?>
             </a>
@@ -30,27 +32,37 @@ $this->Flash->render();
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                    </li>
+                    <!-- Navigation button that links to admin flowers page -->
                     <li class="nav-item" >
                         <?= $this->Html->link('Flowers', ['controller' => 'Flowers', 'action' => 'index'], ['class' => 'nav-link' . ($activePage == 'Flowers' ? ' active' : '')]) ?>
                     </li>
+                    <!-- Navigation button that links to admin payment page -->
                     <li class="nav-item" >
                         <?= $this->Html->link('Payment', ['controller' => 'Payments', 'action' => 'adminIndex'], ['class' => 'nav-link' . ($activePage == 'Payments' ? ' active' : '')]) ?>
                     </li>
+                    <!-- Navigation button that links to admin users page -->
                     <li class="nav-item" >
                         <?= $this->Html->link('Users', ['controller' => 'Users', 'action' => 'index'], ['class' => 'nav-link' . ($activePage == 'Users' ? ' active' : '')]) ?>
                     </li>
+                    <!-- Navigation button that links to admin categories page -->
                     <li class="nav-item" >
                         <?= $this->Html->link('Categories', ['controller' => 'Categories', 'action' => 'index'], ['class' => 'nav-link' . ($activePage == 'Categories' ? ' active' : '')]) ?>
                     </li>
+                    <!-- Navigation button that links to admin orders page -->
                     <li class="nav-item" >
                         <?= $this->Html->link('Orders', ['controller' => 'OrderDeliveries', 'action' => 'index'], ['class' => 'nav-link' . ($activePage == 'OrderDeliveries' ? ' active' : '')]) ?>
                     </li>
+                    <!-- Navigation button that links to admin content blocks page -->
                     <li class="nav-item">
                         <?= $this->Html->link('Content Blocks', ['plugin' => 'ContentBlocks', 'controller' => 'ContentBlocks', 'action' => 'index'], ['class' => 'nav-link' . ($activePage == 'ContentBlocks' ? ' active' : '')]) ?>
                     </li>
+                    <!-- Navigation button that links to customer home page -->
+                    <li class="nav-item">
+                        <?= $this->Html->link('Customer View', ['controller' => 'Pages', 'action' => 'display'], ['class' => 'nav-link' . ($activePage == 'Pages' ? ' active' : '')]) ?>
+                    </li>
                 </ul>
             </div>
+            <!-- Logout button -->
             <div class="d-none d-lg-block" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item <?= ($activePage == 'Auth/logout') ? 'active' : '' ?>">
